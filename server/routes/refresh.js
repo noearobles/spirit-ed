0645cb3b44234cb26915539ceb8c21bd658c0d22
noearobles <roblesnoe1030@gmail.com>
2023-08-31 22:05:58 -0500

    modified directory

--- server/routes/refresh.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const refreshTokenController = require("../controllers/refresh");
-
-const router = express.Router();
-
-router.get("/refresh", refreshTokenController);
-
-module.exports = router;
